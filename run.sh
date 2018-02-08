bf392e18d64ad264274abf67bf638f607bc0e7c3
maoyi <kangmaoyi@100tal.com>
2018-02-08 12:56:58 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+export MKL_THREADING_LAYER=GNU
+THEANO_FLAGS="device=cpu,floatX=float32" ~/zhangzhenhu/env/bin/python train_nea.py \
+	-tr data/AIlab/train.tsv \
+	-tu data/fold_0/dev.tsv \
+	-ts data/AIlab/test.tsv \
+	-p 1 \
+	--epochs 20 \
+	-o output
